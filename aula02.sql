873d23a1fe2443e4354983adc3078e0cb06cecc9
mramalhog <marirg28@gmail.com>
2023-08-11 22:44:45 -0300

    aula-2

---
+++ aula02.sql
@@ -0,0 +1,9 @@
+create database bd_sistema_bancario;
+\l
+create table tabela1 (codigo integer);
+create table tabela2 (cliente char);
+create table tabela3 (conta integer);
+create table tabela4 (agencia integer);
+create table tabela5 (valor float);
+\dt
+\d tabela1
